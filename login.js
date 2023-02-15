65a132ce8882cb0c1424a9a8e2359067783fd6fe
Bean Curd <S10242566@connect.np.edu.sg>
2023-02-15 16:43:13 +0800

    Finalised login page API

---
+++ login.js
@@ -0,0 +1,37 @@
+const APIKEY = "63e531fa478852088da67f67";
+const clicklogin = (event) => {
+  /*The message below the login form*/
+  event.preventDefault(); /*Prevent page from refreshing after text is entered*/
+
+  var username = $("#username").val();
+  var password = $("#pwd").val();
+
+  let getaccount = {
+    async: true,
+    crossDomain: true,
+    url: "https://ipaccountinfos-e395.restdb.io/rest/accounts",
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "x-apikey": APIKEY,
+      "cache-control": "no-cache",
+    },
+  };
+
+  $.ajax(getaccount).done(function (response) {
+    for (var i = 0; i < response.length; i++) {
+      if (response[i].email == username && response[i].password == password) {
+        /*If the information is correct*/
+        window.location.href = "http://127.0.0.1:5500/homepage.html";
+      } else if (
+        response[i].email !== username &&
+        response[i].password !== password
+      ) {
+        $("#msg") /*If the information is wrong*/
+          .html(`<div class="container p-3 text-danger-emphasis bg-danger-subtle border border-danger-subtle rounded-3">
+                            **Invalid email or password, please try again.
+                            </div>`);
+      }
+    }
+  });
+};
