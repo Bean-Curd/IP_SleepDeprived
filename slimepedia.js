79849f37e0b994c20e4f6938c842460e956ed499
Bean Curd <S10242566@connect.np.edu.sg>
2023-02-19 00:16:47 +0800

    Fixed IDs

--- slimepedia.js
+++ slimepedia.js
@@ -1,12 +1,12 @@
 document.getElementById("MYspopt1").onclick = () => {
   window.location.href = "http://127.0.0.1:5500/slimepedia_V.html";
 };
-document.getElementById("MYspopt1").onclick = () => {
+document.getElementById("MYspopt2").onclick = () => {
   window.location.href = "http://127.0.0.1:5500/slimepedia_Z.html";
 };
-document.getElementById("MYspopt1").onclick = () => {
+document.getElementById("MYspopt3").onclick = () => {
   window.location.href = "http://127.0.0.1:5500/slimepedia_R.html";
 };
-document.getElementById("MYspopt1").onclick = () => {
+document.getElementById("MYspopt4").onclick = () => {
   window.location.href = "http://127.0.0.1:5500/slimepedia_N.html";
 };
