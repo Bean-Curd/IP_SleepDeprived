4749615043f72a7214d647bb61112bc30d788a53
Bean Curd <S10242566@connect.np.edu.sg>
2023-02-19 00:16:07 +0800

    Slimepedia Entries

--- slimepedia.js
+++ slimepedia.js
@@ -1,3 +1,12 @@
 document.getElementById("MYspopt1").onclick = () => {
-  window.location.href = "http://127.0.0.1:5500/slimepedia_content.html";
+  window.location.href = "http://127.0.0.1:5500/slimepedia_V.html";
+};
+document.getElementById("MYspopt1").onclick = () => {
+  window.location.href = "http://127.0.0.1:5500/slimepedia_Z.html";
+};
+document.getElementById("MYspopt1").onclick = () => {
+  window.location.href = "http://127.0.0.1:5500/slimepedia_R.html";
+};
+document.getElementById("MYspopt1").onclick = () => {
+  window.location.href = "http://127.0.0.1:5500/slimepedia_N.html";
 };
